3b579c4e5e4a60cd36222969629e4c22f6d774ad
admin <s-taji@connectcrew.co.jp>
2022-02-18 13:49:47 +0900

    定義パスを追加

--- src/main/java/com/example/demo/WorkdaysProperties.java
+++ src/main/java/com/example/demo/WorkdaysProperties.java
@@ -5,6 +5,7 @@ public class WorkdaysProperties {
     public static String inputFolder = "C:/pleiades/workdays/workdays/src/main/resources/PropertyFiles";
     public static String outputFolder = "C:/pleiades/workdays/workdays/src/main/resources/OutputFiles";
     public static String outputFileName = "勤怠表.xls";
+    public static String cccExcelFile = "/apache/htdocs/image/test.xlsx";
 
     //久保さん
 	// String inputFolder = "C:/久保さん/PropertyFiles";
