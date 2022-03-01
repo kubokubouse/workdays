76d5b0479f5a6eaa8d6dbe7bc2c2a2e000015efa
kubokubouse <r-kubo@connectcrew.co.jp>
2022-03-01 14:44:28 +0900

    利用可否修正

---
+++ src/main/java/com/example/demo/model/IdUser.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
+public class IdUser {
+   
+    int companyID;
+
+	 
+	String company1;
+
+	 
+	String company2;
+ 
+	String company3;
+
+	Integer banned;
+
+    String companyName;
+
+	
+	  
+}
