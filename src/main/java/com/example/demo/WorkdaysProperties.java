11c9c2beab98503d1b9e6049d492c534f22e51ab
kubokubouse <r-kubo@connectcrew.co.jp>
2022-03-04 15:06:40 +0900

    テキストファイルへのパスの追加

--- src/main/java/com/example/demo/WorkdaysProperties.java
+++ src/main/java/com/example/demo/WorkdaysProperties.java
@@ -7,12 +7,13 @@ import java.sql.Timestamp;
 public class WorkdaysProperties {
 
     public static String basePath = "C:/pleiades/workdays/workdays/src/main/resources";
-    // public static String basePath = "C:/久保さん/cloneproject/workdays/src/main/resources
+    // public static String basePath = "C:/久保さん/cloneproject/workdays/src/main/resources";
     public static String outputFileName = "勤怠表.xls";
     public static String cccExcelFile = "/apache/htdocs/image/test.xlsx";
     public static String fromMailAdress="r-kubo@connectcrew.co.jp";
     public static String userRegisterText="/mail/userregister.txt";
     public static String loginText="/mail/login.txt";
+    public static String onetimeText="/mail/onetime.txt";
 
     
     public File getInputFolder(int companyID) {
