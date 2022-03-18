6019d05557fc71af748e1367e7bd516a1fdf07b7
kubokubouse <r-kubo@connectcrew.co.jp>
2022-03-18 17:37:25 +0900

    祝日ファイルアップロード機能追加

--- src/main/java/com/example/demo/controller/MasterController.java
+++ src/main/java/com/example/demo/controller/MasterController.java
@@ -1,8 +1,18 @@
 package com.example.demo.controller;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.validation.BindingResult;
@@ -14,8 +24,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.WorkdaysProperties;
 import com.example.demo.model.ContractData;
 import com.example.demo.model.Holiday;
 import com.example.demo.model.Login;
@@ -283,10 +296,8 @@ public class MasterController {
 		YearMonth yearMonth=userService.nowYearMonth();
 		session.setAttribute("yearMonth",yearMonth);
 		model.addAttribute("yearMonth", yearMonth);
-        List<Holiday>holidayList=holidayService.findyear(yearMonth.getYear());
-		//月日で昇順ソート
-		holidayList.sort(Comparator.comparing(Holiday::getMonth)
-		.thenComparing(Comparator.comparing(Holiday::getDay)));
+		//その年の昇順ソートされたリストを入手
+        List<Holiday>holidayList=holidayService.getholidayList(yearMonth.getYear());
         model.addAttribute("holidayList", holidayList);
 
 		return "holidaylist";
@@ -297,10 +308,8 @@ public class MasterController {
 	public String holidaydelete(@RequestParam("id") String id, Model model){
 		holidayService.delete(Integer.parseInt(id));
 		YearMonth yearMonth=(YearMonth)session.getAttribute("yearMonth");
-		List<Holiday>holidayList=holidayService.findyear(yearMonth.getYear());
-		//月日で昇順ソート
-		holidayList.sort(Comparator.comparing(Holiday::getMonth)
-		.thenComparing(Comparator.comparing(Holiday::getDay)));
+		//その年の昇順ソートされたリストを入手
+        List<Holiday>holidayList=holidayService.getholidayList(yearMonth.getYear());
         model.addAttribute("holidayList", holidayList);
 		model.addAttribute("yearMonth", yearMonth);
 		return "holidaylist";
@@ -319,10 +328,8 @@ public class MasterController {
 	public String holidayupdate(Model model,@ModelAttribute Holiday holiday, BindingResult result){
 		holidayService.update(holiday);
 		YearMonth yearMonth=(YearMonth)session.getAttribute("yearMonth");
-		List<Holiday>holidayList=holidayService.findyear(yearMonth.getYear());
-		//月日で昇順ソート
-		holidayList.sort(Comparator.comparing(Holiday::getMonth)
-		.thenComparing(Comparator.comparing(Holiday::getDay)));
+		//その年の昇順ソートされたリストを入手
+        List<Holiday>holidayList=holidayService.getholidayList(yearMonth.getYear());
         model.addAttribute("holidayList", holidayList);
 		return "holidaylist";
 	}
@@ -343,15 +350,14 @@ public class MasterController {
 		System.out.println(oneholiday);
 		holidayService.insert(oneholiday);
 		YearMonth yearMonth=(YearMonth)session.getAttribute("yearMonth");
-		List<Holiday>holidayList=holidayService.findyear(yearMonth.getYear());
-		//月日で昇順ソート
-		holidayList.sort(Comparator.comparing(Holiday::getMonth)
-		.thenComparing(Comparator.comparing(Holiday::getDay)));
+		//その年の昇順ソートされたリストを入手
+        List<Holiday>holidayList=holidayService.getholidayList(yearMonth.getYear());
         model.addAttribute("holidayList", holidayList);
 		model.addAttribute("yearMonth", yearMonth);
 		return "holidaylist";
 	}
 
+	//1年の祝日全削除
 	@RequestMapping(value="/holidayalldelete")
 	public String holidayalldelete(@RequestParam("year") String year, Model model,@ModelAttribute Holiday holiday){
 		List<Holiday>holidayList=holidayService.findyear(Integer.parseInt(year));
@@ -360,17 +366,172 @@ public class MasterController {
 		}
 
 		YearMonth yearMonth=(YearMonth)session.getAttribute("yearMonth");
-		List<Holiday>holidayList2=holidayService.findyear(yearMonth.getYear());
-		//月日で昇順ソート
-		holidayList.sort(Comparator.comparing(Holiday::getMonth)
-		.thenComparing(Comparator.comparing(Holiday::getDay)));
+		//その年の昇順ソートされたリストを入手
+        List<Holiday>holidayList2=holidayService.getholidayList(yearMonth.getYear());
         model.addAttribute("holidayList", holidayList2);
 		model.addAttribute("yearMonth", yearMonth);
 		return "holidaylist";
 	}
 
+	//年度の選択
+	@RequestMapping(value="/selectyear")
+	public String selectyear(@RequestParam("nowyear") String nowyear, Model model){
+		int year=userService.nowYearMonth().getYear()+Integer.parseInt(nowyear)-1;
+		YearMonth yearMonth=(YearMonth)session.getAttribute("yearMonth");
+		yearMonth.setYear(year);
+		session.setAttribute("yearMonth", yearMonth);
+		//その年の昇順ソートされたリストを入手
+        List<Holiday>holidayList=holidayService.getholidayList(yearMonth.getYear());
+        model.addAttribute("holidayList", holidayList);
+		model.addAttribute("yearMonth", yearMonth);
+		return "holidaylist";
+	}
 
+	//祝日アップロード画面へ遷移
+	@GetMapping("/holidayupload")
+    public String holidayUpload() {
 
+		MasterUser masterUser = (MasterUser)session.getAttribute("masterUser");
+        if(masterUser==null){
+            return "masterloginfault";
+        }
+		return "holidayupload";
+	}
+	//祝日一括アップロード処理
+    @RequestMapping("/uploadholiday")
+    public String uploadalluser(@RequestParam("file") MultipartFile multipartFile,
+    Model model) throws IOException {
+
+		//祝日を全削除する年度を指定
+        YearMonth yearMonth=(YearMonth)session.getAttribute("yearMonth");
+
+        //全ユーザー削除
+        List<Holiday>holidayList=holidayService.findyear(yearMonth.getYear());
+		for(Holiday holidays:holidayList){
+			holidayService.delete((holidays.getId()));
+		}
+
+        // ファイルが空の場合は異常終了
+        if(multipartFile.isEmpty()){
+        // 異常終了時の処理
+            model.addAttribute("error", "ファイルのアップロードに失敗しました"); 
+            return "holidayupload";   
+        }
+        
+        //格納先のフルパス
+        String filePath = WorkdaysProperties.basePath + "/uploadholiday.csv";
+
+        try {
+            byte[] bytes = multipartFile.getBytes();
+            BufferedOutputStream uploadFileStream =
+                new BufferedOutputStream(new FileOutputStream(filePath));
+            uploadFileStream.write(bytes);
+            uploadFileStream.close();  
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // アップロードファイルから値を読み込んで保存する
+        BufferedReader br = null;
+        File uploadFile = new File(filePath);
+
+        if(!uploadFile.exists()) {
+        // 異常終了時の処理
+            model.addAttribute("error", "ファイルのアップロードに失敗しました"); 
+            return "holidayupload";  
+        }
+
+        int i = 0;
+        List<String> errorMailList = new ArrayList<String>();
+        List<Holiday> hdList = new ArrayList<Holiday>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        try {            
+            br = new BufferedReader(new FileReader(uploadFile));
+            // readLineで一行ずつ読み込む
+            String line; // 読み込み行
+            String[] data; // 分割後のデータを保持する配列
+            while ((line = br.readLine()) != null) {
+
+                if (i == 0) {
+                    //カンマで分割した内容を配列に格納する
+                    String[] header = line.split(",");
+
+                    // Listの中でヘッダー項目が何か
+                    String year = header[0];
+                    //マッピングする
+                    map.put(year, 0);
+
+                    String month = header[1];
+                    map.put(month, 1);
+
+                    String day = header[2];
+                    map.put(day, 2);
+
+                    String name = header[3];
+                    map.put(name, 3);
+                    
+                } else {
+
+                    Holiday holiday = new Holiday();
+
+                // lineをカンマで分割し、配列dataに設定
+                    data = line.split(",");
+
+                    //mapから何番目にデータが格納されているか取得
+                    if (map.containsKey("年")){
+                        int year = map.get("年");
+                        holiday.setYear(Integer.parseInt(data[year]));
+                    } else {
+                        model.addAttribute("error", "ヘッダーの名前を確認してください：年"); 
+                        return "holidayupload";  
+                    }
+                    if (map.containsKey("月")) {
+                        int month = map.get("月");
+                        holiday.setMonth(Integer.parseInt(data[month]));
+                    } else {
+                        model.addAttribute("error", "ヘッダーの名前を確認してください：月"); 
+                        return "holidayupload";  
+                    }
+
+                    if (map.containsKey("日")){
+                        int day = map.get("日");
+						holiday.setDay(Integer.parseInt(data[day]));
+                    } else {
+                        model.addAttribute("error", "ヘッダーの名前を確認してください：日"); 
+                        return "holidayupload";  
+                    }
+
+                    if (map.containsKey("祝日名")){
+                        int name = map.get("祝日名");
+                        holiday.setName(data[name]);
+                    } else {
+                        model.addAttribute("error", "ヘッダーの名前を確認してください：祝日名"); 
+                        return "holidayupload";  
+                    }
+
+                    //idData.setCompanyID(companyID);
+                    hdList.add(holiday);
+                }
+                i++;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            model.addAttribute("error", "ファイルのアップロードに失敗しました"); 
+            return "holidayupload";
+        } finally {
+            br.close();
+            uploadFile.delete();
+        }
+
+        // int companyID,String mail,int individual_id,String name,int banned,int registered, String company1,String company2,String company3,String number
+        for (Holiday holiday : hdList) {
+            holidayService.insert(holiday);
+        }
+          
+        model.addAttribute("error", "ファイルがアップロードされました");
+        return "holidayupload";
+    }
 
 }
 
