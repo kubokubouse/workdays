4bd7237ce703f47b9b796a08d238f77fb5e29023
takebayashi <c-takebayashi@connectcrew.co.jp>
2022-03-22 11:58:02 +0900

    useruploadのbox連携

--- src/main/java/com/example/demo/WorkdaysProperties.java
+++ src/main/java/com/example/demo/WorkdaysProperties.java
@@ -6,9 +6,9 @@ import java.sql.Timestamp;
 
 public class WorkdaysProperties {
 
-    public static String host = "http://localhost:8080";
-    //public static String basePath = "C:/pleiades/workdays/workdays/src/main/resources";
-    public static String basePath = "C:/久保さん/cloneproject/workdays/src/main/resources";
+    public static String host = "http://ec2-54-92-109-108.ap-northeast-1.compute.amazonaws.com:8080";
+    public static String basePath = "C:/pleiades/workdays/workdays/src/main/resources";
+    //public static String basePath = "C:/久保さん/cloneproject/workdays/src/main/resources";
     public static String outputFileName = "勤怠表.xls";
     public static String cccExcelFile = "/apache/htdocs/image/test.xlsx";
     public static String fromMailAdress="r-kubo@connectcrew.co.jp";
@@ -19,7 +19,10 @@ public class WorkdaysProperties {
     public static String boxClientId = "4u7hb7ffjwrpl9k1ojfsf09g58eyqwt6";
 	public static String boxClientSecret = "wE7OWwmsH0V1Z1wYscLQUaOgTpiUqNvJ";
     public static String boxSaveFolderName = "WorkDays";
-
+    //TODO ※templateupload.htmlのリンクを修正すること
+    public static String boxTemplateDownloadUri = "https://account.box.com/api/oauth2/authorize?client_id=4u7hb7ffjwrpl9k1ojfsf09g58eyqwt6&response_type=code&redirect_uri=http://localhost:8080/boxtemplateupload";
+    //TODO ※alluserupload.htmlのリンクを修正すること
+    public static String boxCSVDownloadUri = "https://account.box.com/api/oauth2/authorize?client_id=4u7hb7ffjwrpl9k1ojfsf09g58eyqwt6&response_type=code&redirect_uri=http://localhost:8080/uploaduserfrombox";
     
     public File getInputFolder(int companyID) {
         File inputFile = new File(basePath + "//" + companyID + "_input");
