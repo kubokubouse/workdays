893a70c8b28503a269e9cf4fba070a08259d858d
kubokubouse <r-kubo@connectcrew.co.jp>
2022-03-22 16:06:19 +0900

    メソッド追加

--- src/main/java/com/example/demo/service/IndividualService.java
+++ src/main/java/com/example/demo/service/IndividualService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.CompanyInfo;
+import com.example.demo.model.IdUser;
 import com.example.demo.model.IndividualData;
 import com.example.demo.repository.IndividualDataRepository;
 
@@ -12,6 +15,8 @@ import com.example.demo.repository.IndividualDataRepository;
 public class IndividualService {
     @Autowired
     IndividualDataRepository individualRepository;
+    @Autowired
+    CompanyInfoService companyInfoService;
 
     public List <IndividualData> findMail(String mail){
     	return individualRepository.findByMail(mail);
@@ -19,5 +24,18 @@ public class IndividualService {
    
     public int insert(int companyID, String mail, int individual_id,String name, int banned, int registered, String company1,String company2,String company3){
         return individualRepository.InsertIndividualData(companyID, mail, individual_id,name, banned, registered,company1,company2,company3);
+    }
+    
+    //個別ユーザーデータのデータをidUserに詰め替える+会社名を取得する
+    public IdUser getIdUser(IndividualData iData){
+        IdUser idUser=new IdUser();
+        CompanyInfo companyInfo= companyInfoService.findByCompanyID(iData.getCompanyID());
+		idUser.setCompany1(iData.getCompany1());
+		idUser.setCompany2(iData.getCompany2());
+		idUser.setCompany3(iData.getCompany3());
+		idUser.setBanned(iData.getBanned());
+		idUser.setCompanyID(iData.getCompanyID());
+		idUser.setCompanyName(companyInfo.getCompanyName());
+        return idUser;
     }
 }
