a017d3caed1cb6e34122c93c70ceece34d37b0eb
takebayashi <c-takebayashi@connectcrew.co.jp>
2022-04-14 11:52:49 +0900

    year,monthタグ

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -520,7 +520,8 @@ public class HomeController extends WorkdaysProperties{
 		WorkdayMapping workdayMapping = new WorkdayMapping();
 		List<String> errors = workdayMapping.outputExcel(inputFilePath, outputFilePath, 
 			stHourMap, stMinMap, endHourMap, endMinMap, lunchTimeHourMap, lunchTimeMinMap,
-			totalHourMap, totalMinMap, other1Map,other2Map,other3Map, mail, individualID, name
+			totalHourMap, totalMinMap, other1Map,other2Map,other3Map, mail, individualID, name,
+			year, month
 		);
 
 		if (errors.size() != 0 || !errors.isEmpty()) {
@@ -904,7 +905,8 @@ public String univeresalregist(@Validated @ModelAttribute User user, BindingResu
 		WorkdayMapping workdayMapping = new WorkdayMapping();
 		List<String> errors = workdayMapping.outputExcel(inputFilePath, outputFilePath, 
 			stHourMap, stMinMap, endHourMap, endMinMap, lunchTimeHourMap, lunchTimeMinMap,
-			totalHourMap, totalMinMap, other1Map,other2Map,other3Map, mail, individualID, name
+			totalHourMap, totalMinMap, other1Map,other2Map,other3Map, mail, individualID, name,
+			year, month
 		);
 
 		if (errors.size() != 0 || !errors.isEmpty()) {
