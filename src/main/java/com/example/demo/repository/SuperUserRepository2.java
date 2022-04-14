9221d8f272c71ea85903efe6f6eaf6b242668ed4
kubokubouse <r-kubo@connectcrew.co.jp>
2022-04-14 10:07:50 +0900

    会社ID検索メソッド追加

--- src/main/java/com/example/demo/repository/SuperUserRepository2.java
+++ src/main/java/com/example/demo/repository/SuperUserRepository2.java
@@ -1,6 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 import com.example.demo.model.SuperUser;
 
 public interface SuperUserRepository2 extends JpaRepository<SuperUser, String> {
@@ -8,4 +11,5 @@ public interface SuperUserRepository2 extends JpaRepository<SuperUser, String> {
 	SuperUser findByEmailAndCompanyID(String Email,int companyID);
     SuperUser findById(int Id);
     SuperUser findByEmail(String email);
+    List<SuperUser> findByCompanyID(int companyID);
 }
