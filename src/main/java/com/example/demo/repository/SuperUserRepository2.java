bc388f30546beb2dd77ecc2f74b881e59077db2a
kubokubouse <r-kubo@connectcrew.co.jp>
2022-04-19 17:55:41 +0900

    メール会社ID更新メソッド追加

--- src/main/java/com/example/demo/repository/SuperUserRepository2.java
+++ src/main/java/com/example/demo/repository/SuperUserRepository2.java
@@ -4,10 +4,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import com.example.demo.model.SuperUser;
 
 public interface SuperUserRepository2 extends JpaRepository<SuperUser, String> {
-	
+	@Transactional
+	@Modifying
+	@Query(value ="update individual_data set email=?1 where id=?2  " ,
+
+	        nativeQuery = true)
+    int updateEmail(String email,int id);
+    
+    @Transactional
+	@Modifying
+	@Query(value ="update individual_data set copmanyID=?1 where id=?2  " ,
+
+	        nativeQuery = true)
+    int updateCompanyID(int companyID,int id);
+    
+
 	SuperUser findByEmailAndCompanyID(String Email,int companyID);
     SuperUser findById(int Id);
     SuperUser findByEmail(String email);
