bfaea00d86c260dc4d3c6c49ce1406a391e83f19
kubokubouse <r-kubo@connectcrew.co.jp>
2022-05-11 11:34:24 +0900

    IP全部足す

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -103,6 +103,7 @@ public class HomeController extends WorkdaysProperties{
 	public String tester(){
 		System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, "/var/www/html/download/chromedriver" /*"C:/久保さん/cloneproject/workdays/chromedriver.exe"*/);
 
+		System.setProperty("webdriver.chrome.whitelistedIps", "");
         //Chromeドライバーのインスタンス
         WebDriver driver = new ChromeDriver();
 
